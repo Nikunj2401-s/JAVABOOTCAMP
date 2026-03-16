aeed39cf0cf0c64c3330306ef7ecd89862f1b399
Nikunj2401-s <nikunjbhard001@gmail.com>
2026-03-16 10:15:55 +0530

    Updated BookMystay App

---
+++ SEM 4/BookMyStay/UC5/Reservation.java
@@ -0,0 +1,25 @@
+package SEM 4.BookMyStay.UC5;
+
+
+public class Reservation {
+    private String guestName;
+    private String roomType;
+
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation [Guest: " + guestName + ", Room Type: " + roomType + "]";
+    }
+}
